7fb16229a6e9983c5a3328edd88981beeb96182c
alexdolya <alexdolya90@gmail.com>
2024-07-04 10:57:50 +0400

    init

---
+++ src/test/java/ru/dolya/fortest/ForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolya.fortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
